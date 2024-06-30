59fdb5b0b466a438b1c69642185d0ae7702589cc
Prabal singh Parihar <aprabal257730@gmail.com>
2024-06-30 11:33:25 +0530

    3rd

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const path = require(('path'))
 var bodyParser = require('body-parser');
 const app= express();
 const cors = require('cors')
+
+
 const port =5000;
 // const port = process.env.PORT
 
@@ -10,6 +12,7 @@ const port =5000;
 app.use(cors());
 app.use(bodyParser.json());
 app.use('/', require('./routes/index'));
+
 
 app.listen(port,(err)=>{
     if(err){
@@ -35,7 +38,7 @@ app.listen(port,(err)=>{
 // make a connection to the front end//
 
 // app.use((req,res,next)=>{
-//   res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Origin","https://fdapp-nsrc.vercel.app/");
 //   res.header(
 //     'Access-Control-Allow-Headers',
 //     'Origin,X-Requested-With, Content-Type, Accept'
