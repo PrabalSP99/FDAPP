abcf3ecf9e2d38c257269a31ac75055e1d31370f
Prabal singh Parihar <aprabal257730@gmail.com>
2024-06-30 22:19:02 +0530

    7th commit

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,29 @@
 const express =require('express');
+const mongoose = require('mongoose');
 const path = require(('path'))
 var bodyParser = require('body-parser');
 const app= express();
 const cors = require('cors')
 
+require("dotenv").config();
 
-const port =5000;
-// const port = process.env.PORT
+// middleware
+const corsOptions = {
+    origin: "http://localhost:3000" // frontend URI (ReactJS)
+}
+app.use(express.json());
+app.use(cors(corsOptions));
+
+// connect MongoDB
+mongoose.connect(process.env.mongodb).then(() => {
+    const PORT = process.env.PORT || 5000
+    app.listen(PORT, () => {
+        console.log(`App is Listening on PORT ${PORT}`);
+    })
+}).catch(err => {
+    console.log(err);
+});
+
 
 
 app.use(cors());
@@ -14,34 +31,17 @@ app.use(bodyParser.json());
 app.use('/', require('./routes/index'));
 
 
-app.listen(port,(err)=>{
-    if(err){
-        console.log(`error: ${err}`);
-        return;
-    }
-    console.log(`server running on port : ${port}`)
-})
-
-
-
-
-
-
-
-// static file //
-// app.use(express.static(path.join(__dirname,"./frontend/build")))
-// app.get('*',(req,res)=>{
-//   res.sendFile(path.join(__dirname,'./frontend/build/index.html'))
+// app.listen(port,(err)=>{
+//     if(err){
+//         console.log(`error: ${err}`);
+//         return;
+//     }
+//     console.log(`server running on port : ${port}`)
 // })
 
 
-// make a connection to the front end//
 
-// app.use((req,res,next)=>{
-//   res.setHeader("Access-Control-Allow-Origin","https://fdapp-nsrc.vercel.app/");
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin,X-Requested-With, Content-Type, Accept'
-//   );
-//   next();
-// })
+
+
+
+
