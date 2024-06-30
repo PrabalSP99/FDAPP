123502a84929aa6492a1d01c3f58238564cab79d
Prabal singh Parihar <aprabal257730@gmail.com>
2024-06-30 22:31:07 +0530

    7th commit

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ const cors = require('cors')
 require("dotenv").config();
 
 // middleware
-const corsOptions = {
-    origin: "http://localhost:3000" // frontend URI (ReactJS)
-}
-app.use(express.json());
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: "http://localhost:3000" // frontend URI (ReactJS)
+// }
+// app.use(express.json());
+// app.use(cors(corsOptions));
 
 // connect MongoDB
 mongoose.connect(process.env.mongodb).then(() => {
